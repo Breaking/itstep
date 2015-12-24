d10c570f3d55313700b87efdc6b35f925b376af1
Breaking <makler_cs@tut.by>
2015-12-24 21:11:05 +0300

    24_12 end lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,43 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
-        System.out.println("Hello world2");
+        int[] m1 = new int[100];
+        //int[] m2 = {1, 2, 3};
+        Random random = new Random();
+
+        for(int i = 0; i < m1.length; i++){
+            m1[i] = -500 + random.nextInt(1000);
+        }
+
+        for (int i = 0; i < m1.length; i++){
+            System.out.println(i+". "+m1[i]);
+        }
+        System.out.println("------------------------------");
+
+
+        int[] m2 = new int[100];
+        int j = 0;
+
+        for (int i = 0; i < m2.length; i++){
+            if (m1[i] > 0 && i % 2 == 0){
+                m2[j] = m1[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < m2.length; i++){
+            System.out.println(i+"." + m2[i]);
+        }
+
+
+
+        System.out.println();
+        //System.out.println("Hello world");
+        //System.out.println("Hello world2");
     }
 }
