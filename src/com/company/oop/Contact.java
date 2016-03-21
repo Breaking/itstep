b2702869ffe3ed8b91f3e559eab7cc7421b918e8
Breaking <makler_cs@tut.by>
2016-03-21 23:00:30 +0300

    ContactManager OOP. begin

---
+++ src/com/company/oop/Contact.java
@@ -0,0 +1,73 @@
+package com.company.oop;
+
+/**
+ * Created by Mike on 12.03.2016.
+ */
+public class Contact {
+    private String name;
+    private String phone;
+    private String address;
+    private int dayOfBirthday;
+    private int monthOfBirthday;
+    private int yearOfBirthday;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phone, String address, int dayOfBirthday, int monthOfBirthday, int yearOfBirthday) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.dayOfBirthday = dayOfBirthday;
+        this.monthOfBirthday = monthOfBirthday;
+        this.yearOfBirthday = yearOfBirthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getDayOfBirthday() {
+        return dayOfBirthday;
+    }
+
+    public void setDayOfBirthday(int dayOfBirthday) {
+        this.dayOfBirthday = dayOfBirthday;
+    }
+
+    public int getMonthOfBirthday() {
+        return monthOfBirthday;
+    }
+
+    public void setMonthOfBirthday(int monthOfBirthday) {
+        this.monthOfBirthday = monthOfBirthday;
+    }
+
+    public int getYearOfBirthday() {
+        return yearOfBirthday;
+    }
+
+    public void setYearOfBirthday(int yearOfBirthday) {
+        this.yearOfBirthday = yearOfBirthday;
+    }
+}
---
+++ src/com/company/oop/ContactsManager.java
@@ -0,0 +1,112 @@
+package com.company.oop;
+
+import com.company.Main;
+
+import java.util.Scanner;
+
+/**
+ * Created by Mike on 12.03.2016.
+ */
+public class ContactsManager {
+    public static int MAX_RECORDS = 5;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean[] ids = new boolean[MAX_RECORDS];
+        Contact[] contacts = new Contact[MAX_RECORDS];
+
+        while (true) {
+            System.out.println("*******Contacts manager********");
+            System.out.println("Enter your choice: ");
+            System.out.println("1-Add");
+            System.out.println("2-Delete");
+            System.out.println("3-Search");
+            System.out.println("4-View all");
+            System.out.println("5-Sort contacts");
+            System.out.println("0-Exit");
+
+            String mainMenu = scanner.nextLine();
+
+            if ("0".equals(mainMenu)) {
+                System.out.println("Bye :)");
+                break;
+            } else if ("1".equals(mainMenu)) {
+                addContact(scanner, ids, contacts);
+            } else if ("2".equals(mainMenu)) {
+
+            } else if ("3".equals(mainMenu)) {
+
+            } else if ("4".equals(mainMenu)) {
+                viewAllContacts(contacts);
+            } else {
+                System.out.println("Incorrect input");
+            }
+
+        }
+
+    }
+
+    public static void printHeader() {
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("| ID |        NAME       |    PHONE    |         ADDRESS         |  BIRTHDAY  |");
+        System.out.println("-------------------------------------------------------------------------------");
+
+    }
+
+    public static void printFooter() {
+        System.out.println("-------------------------------------------------------------------------------");
+    }
+
+
+    public static void addContact(Scanner scanner, boolean[] ids, Contact[] contacts) {
+        System.out.println("|Adding|");
+        System.out.println("Enter name");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone");
+        String phone = scanner.nextLine();
+        System.out.println("Enter address");
+        String address = scanner.nextLine();
+        System.out.println("Enter date of birthday:");
+        System.out.println("Enter day");
+        int dayOfBirthday = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter month");
+        int monthOfBirthday = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter year");
+        int yearOfBirthday = Integer.valueOf(scanner.nextLine());
+
+        Contact contact = new Contact(name, phone, address, dayOfBirthday, monthOfBirthday, yearOfBirthday);
+
+        contacts[findId(ids)] = contact;
+
+    }
+
+    public static void viewAllContacts(Contact[] contacts) {
+        System.out.println("All records: ");
+        printHeader();
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] != null) {
+                System.out.printf("|" + "%5s", i + "|");
+                System.out.printf("%20s", contacts[i].getName() + "|");
+                System.out.printf("%14s", contacts[i].getPhone() + "|");
+                System.out.printf("%26s", contacts[i].getAddress() + "|");
+                System.out.printf("%13s", contacts[i].getDayOfBirthday() + "." + contacts[i].getMonthOfBirthday() +
+                        "." + contacts[i].getYearOfBirthday() + "|");
+                System.out.println();
+            } else
+                break;
+
+        }
+        printFooter();
+    }
+
+    //Searching place(id) for a new contact
+    public static int findId(boolean[] ids) {
+        for (int i = 0; i < ids.length; i++) {
+            if (!ids[i]) {
+                return i;
+            }
+        }
+        System.out.println("ERROR");
+        return -1;
+    }
+}
